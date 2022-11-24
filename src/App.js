87b05564e2c5ebbee59b050497f06e4f81471055
JC4F <lequan12122002@gmail.com>
2022-11-24 21:15:27 +0700

    setup redux cho du an

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { useDispatch, useSelector } from 'react-redux';
 import Header from './components/Header/Header'
+import { decreaseCounter, increaseCounter } from './redux/action/counterAction';
 
 const App = ()=>{
+  const dispatch = useDispatch();
+  const count = useSelector(state => state.counter.count);
+
   return (
     <div className="App">
       <Header></Header>
+      <div>Count: {count}</div>
+      <button onClick={() => dispatch(increaseCounter())}>Increase Count</button>
+      <button onClick={() => dispatch(decreaseCounter())}>Decrease Count</button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -10,18 +10,22 @@ import {
 } from "react-router-dom";
 import About from './components/About/About'
 import AppRoute from './routes/AppRoute'
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<AppRoute/>}>
-          <Route path='about' element={<About/>}/>
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<AppRoute/>}>
+            <Route path='about' element={<About/>}/>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
