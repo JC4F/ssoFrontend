483ea5465fb3229ebc6829c5fe6afc0e9971fd32
JC4F <lequan12122002@gmail.com>
2022-11-25 15:45:26 +0700

    customize axios + jwt vs refresh token -> cookie + expired

--- src/components/Code/Code.js
+++ src/components/Code/Code.js
@@ -1,11 +1,13 @@
-import axios from "axios";
-import { useEffect, useRef } from "react";
-import { useSearchParams } from "react-router-dom";
+import axios from "../../customize/axios";
+import { useEffect, useRef, useState } from "react";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 
 const Code = (props) => {
     const [searchParams, setSearchParams] = useSearchParams();
     const firstRunRef = useRef(false);
+    const [message, setMessage] = useState("");
+    const navigate = useNavigate();
 
     useEffect(()=>{
         const ssoToken = searchParams.get('ssoToken');
@@ -16,7 +18,12 @@ const Code = (props) => {
                 {ssoToken},
                 { withCredentials: true }
             ).then(res=>{
-                console.log(">>>res: ", res);
+                if(res && +res.EC === 0){
+                    //success
+                    navigate('/')
+                }else {
+                    setMessage(res.EM)
+                }
             }).catch(err=>{
                 console.log(">>> Error: ", err);
             })
@@ -24,9 +31,17 @@ const Code = (props) => {
     }, [])
     
     return (
-    <>
-        Code
-    </>
+        <>
+            <div className="container">
+                <div className="row">
+                    <div className="col-12 mt-3">
+                        {message}
+                        {message && <span>. Please do login again. Click here to &nbsp;
+                            <a href={`${process.env.REACT_APP_BACKEND_SSO}?serviceURL=${process.env.REACT_APP_SERVICE_URL}`}>Login</a></span>}
+                    </div>
+                </div>
+            </div>
+        </>
     );
 }
  
